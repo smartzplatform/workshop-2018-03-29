c84b0b33153bdb99c16e10e0a7c5aeda4b12f7a2
quantum13 <quant13@gmail.com>
2018-03-28 19:18:54 +0300

    settings

--- truffle.js
+++ truffle.js
@@ -1,4 +1,22 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 9545,
+            network_id: "*" // match any network
+        },
+        rinkeby: {
+            host: "127.0.0.1",
+            port: 8544,
+            network_id: 4,
+            gasPrice: 1,
+            //from: ''
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
 };
